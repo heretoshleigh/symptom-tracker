8a3b94dfa65034f52fb44d442fe94d95388f282c
Ashleigh Halverstadt <ashleigh.halverstadt@gmail.com>
2022-08-24 13:12:02 -0600

    fixed cors errors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
+const cors = require('cors')
 const PORT = 2121
 const ObjectId = require('mongodb').ObjectID
 const methodOverride = require('method-override')
@@ -20,6 +21,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 
 //METHOD OVERRIDE
 app.use(methodOverride(function (req, res) {
